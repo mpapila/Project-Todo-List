a0e8a3ffe1e5bfea957931b4d9512ebd699d7bd1
mpapila <mehmetpapila95@gmail.com>
2024-03-27 14:52:00 -0500

    added form for adding new to do

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("HELLO")
+import './style.css';
+
+var formVisible = false;
+
+const showFormButton = document.getElementById("addButton");
+showFormButton.addEventListener('click', () => {
+    let form = document.getElementById("todo-form");
+    if (!formVisible) {
+        form.style.display = "block";
+        formVisible = true;
+    } else {
+        formVisible = false;
+        form.style.display = "none";
+    }
+})
+
