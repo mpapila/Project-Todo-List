cacdbe19af820c9d90e1f99b1de30b6f941723dc
mpapila <mehmetpapila95@gmail.com>
2024-03-27 13:07:23 -0500

    installing webpack initializing all necessary files and folders

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path'); // +
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // *
+
+
+module.exports = {// +
+  entry: './src/index.js',// +
+  mode: 'development',
+  output: {// +
+    filename: 'main.js',// +
+    path: path.resolve(__dirname, 'dist'),// +
+    clean: true, // *
+  },// +
+  plugins: [ // *
+    new HtmlWebpackPlugin({ // *
+      title: 'ToDo List', // *
+      template: './src/index.html', // *
+    }), // *
+  ], // *
+};// +
